00787748385fffd4741b1f5b86f914b9b227ba98
BuildTools <mwhaley@csumb.edu>
2019-11-26 17:59:45 -0800

    pojo stuff finished

--- src/main/java/Twocan/TwocanBackend/model/Friends.java
+++ src/main/java/Twocan/TwocanBackend/model/Friends.java
@@ -4,7 +4,23 @@ package Twocan.TwocanBackend.model;
 public class Friends{
 
 
-String username;
-String uid;
+    String username;
+    String uid;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
 
 }
