c1db98d0273e7155f4eed583e07adb79979b5c5f
BuildTools <mwhaley@csumb.edu>
2019-11-30 19:19:12 -0800

    fixed user object

---
+++ User/src/main/java/two/can/User/model/Friends.java
@@ -0,0 +1,26 @@
+package two.can.User.model;
+
+
+public class Friends{
+
+
+    String username;
+    String uid;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUid() {
+        return this.uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+}
--- User/src/main/java/two/can/User/model/User.java
+++ User/src/main/java/two/can/User/model/User.java
@@ -12,7 +12,9 @@ public class User {
     String username;
     String password;
     //@ApiModelProperty(required = false, hidden = true)
-    ArrayList<String> friendslist = new ArrayList<String>();
+    ArrayList<Friends> friendslist = new ArrayList<Friends>();//friends list
+    ArrayList<Group> grouplist = new ArrayList<Group>();//group list
+
     
     public String getUsername() {
         return this.username;
@@ -30,16 +32,29 @@ public class User {
         this.password = password;
     }
     
-    public ArrayList<String> getFriends(){
+    public ArrayList<Friends> getFriends(){
         return this.friendslist;
     }
     
-    public void setFriends(ArrayList<String> list){
+    public void setFriends(ArrayList<Friends> list){
         list = this.friendslist;
     }
     
-    public void addFriend(String friend){
+    public void addFriend(Friends friend){
         friendslist.add(friend);
+    }
+
+
+    public ArrayList<Group> getGroup(){
+        return this.grouplist;
+    }
+    
+    public void setGroup(ArrayList<Group> list){
+        list = this.grouplist;
+    }
+    
+    public void addGroup(Group group){
+        grouplist.add(group);
     }
         
 }
